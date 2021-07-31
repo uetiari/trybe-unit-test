69ebe81e232b95be52c34b3693d74ca253d650a7
Ariane Ueti <arianeueti@gmail.com>
2021-07-31 14:21:25 -0300

    Requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -14,7 +14,14 @@
     estudante.name // Retorna: 'Leandrão, o Lobo Solitário'
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
+const feedBackfn = () => 'Eita pessoa boa!';
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const student = {
+    name: nome,
+    feedback: feedBackfn,
+  };
+  return student;
+};
 
 module.exports = createStudent;
