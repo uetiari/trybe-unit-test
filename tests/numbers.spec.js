43451aeaa3d4cf927a15a5e80cf33fe3a2e71a30
Ariane Ueti <arianeueti@gmail.com>
2021-07-31 11:48:59 -0300

    Requisito 2

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -18,10 +18,15 @@ const numbers = require('../src/numbers');
 
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   it('Verifica se a função `numbers`retorna `true` quando o array contém apenas numeros e falso caso contrário', () => {
-    assert.fail();
+    // assert.fail();
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
+    assert.strictEqual(numbers([1, 2, 3, 4, 5]), true);
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
+    assert.strictEqual(numbers([1, 2, '3', 4, 5]), false);
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
+    assert.strictEqual(numbers([1, 'a', 3]), false);
     // Escreva um teste em que a função recebe [' '] e retorna false
+    assert.strictEqual(numbers([' ']), false);
   });
 });
+;
