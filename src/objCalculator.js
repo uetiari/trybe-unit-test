da6c062ecc776c51de69ad1d8a3a3cdc284e47ba
Ariane Ueti <arianeueti@gmail.com>
2021-07-31 15:06:00 -0300

    Requisito 7

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,6 +19,11 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
+const calculator = {
+  add: (int1, int2) => int1 + int2,
+  mult: (int1, int2) => int1 * int2,
+  div: (int1, int2) => Math.floor(int1 / int2),
+  sub: (int1, int2) => int1 - int2,
+};
 
 module.exports = calculator;
